54d5a0ae3ac633c1ef4f4fee43c385d0961057dd
rd18 <r1111111r@gmail.com>
2024-06-21 13:54:45 +0530

    try job summary in JS

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -1,3 +1,6 @@
+import * as core from '@actions/core'
+
+  
 const request = require("request-promise");
 
 const options = {
@@ -12,7 +15,22 @@ const options = {
 
 async function getJoke() {
   const res = await request(options);
+
+  await core.summary
+.addHeading('Test Results')
+.addCodeBlock(generateTestResults(), "js")
+.addTable([
+  [{data: 'File', header: true}, {data: 'Result', header: true}],
+  ['Joke', res.joke],
+  ['bar.js', 'Fail ❌'],
+  ['test.js', 'Pass ✅']
+])
+.addLink('View staging deployment!', 'https://github.com')
+.write()
+
   return res.joke;
 }
+
+
 
 module.exports = getJoke;
